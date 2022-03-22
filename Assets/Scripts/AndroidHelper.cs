1550b734971b7a2eb3277e7e6a60b34aae86a25b
syaiful <syaiful919@gmail.com>
2022-03-22 22:14:35 +0700

    communication with activity

---
+++ Assets/Scripts/AndroidHelper.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+namespace AndroidHelper
+{
+    public class Listener
+    {
+        class Methods
+        {
+            public const string onPlayerInitialized = "onPlayerInitialized";
+        }
+
+        public static void OnPlayerInitialized()
+        {
+            if(Application.platform == RuntimePlatform.Android)
+            {
+                Android.GetCurrentActivity().Call(Methods.onPlayerInitialized);
+            }
+        }
+    }
+
+    public class Android
+    {
+        public static AndroidJavaObject GetCurrentActivity()
+        {
+            using (var player = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+            {
+                return player.GetStatic<AndroidJavaObject>("currentActivity");
+            }
+        }
+
+        public static string AndroidGetIntentStringExtra(string key)
+        {
+            using (var activity = GetCurrentActivity())
+            {
+                using (var intent = activity.Call<AndroidJavaObject>("getIntent"))
+                {
+                    return intent.Call<string>("getStringExtra", key);
+                }
+            }
+        }
+
+        public static int AndroidGetIntentIntExtra(string key, int defaultVal)
+        {
+            using (var activity = GetCurrentActivity())
+            {
+                using (var intent = activity.Call<AndroidJavaObject>("getIntent"))
+                {
+                    return intent.Call<int>("getIntExtra", key, defaultVal);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using AndroidHelper;
+
+public class GameManager : MonoBehaviour
+{
+    public Text countText;
+    public Button countButton;
+    public Button resetButton;
+    public Text greetingText;
+
+    private int countNumber = 0;
+
+    private AndroidJavaObject activity;
+
+    private void Awake() {
+        Debug.Log("/// [GameManager] Awake");
+
+        if(Application.platform == RuntimePlatform.Android)
+        {
+            activity = Android.GetCurrentActivity();
+            if(activity != null) 
+            {
+                activity.Call("onPreloadGame", "Hello, i'm from unity");
+            }
+        }
+    }
+
+    private void Start() {
+        Debug.Log("/// [GameManager] Start");
+        if(Application.platform == RuntimePlatform.Android)
+        {
+            string extra = Android.AndroidGetIntentStringExtra("greeting");
+            if (extra != null)
+            {
+                greetingText.text = "You have a message: " + extra;
+            }
+        }
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log("/// [GameManager] OnDisable");
+    }
+
+    private void OnDestroy() {
+        Debug.Log("/// [GameManager] OnDestroy");
+    }
+
+    private void OnApplicationQuit() {
+        Debug.Log("/// [GameManager] OnApplicationQuit");
+    }
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+    
+    public void Count()
+    {
+        countNumber++;
+        countText.text = countNumber.ToString();
+    }
+
+    public void Reset() 
+    {
+        countNumber = 0;
+        countText.text = countNumber.ToString();
+    }
+    
+}
