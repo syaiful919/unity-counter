e033e7a24b427fbe76db779c6aad101bd1f37e11
syaiful <syaiful919@gmail.com>
2022-03-22 11:11:31 +0700

    add logic

---
+++ Assets/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text countText;
+    public Button countButton;
+    public Button resetButton;
+
+    private int countNumber = 0;
+
+    public void Count()
+    {
+        countNumber++;
+        countText.text = countNumber.ToString();
+    }
+
+    public void Reset() 
+    {
+        countNumber = 0;
+        countText.text = countNumber.ToString();
+    }
+    
+}
