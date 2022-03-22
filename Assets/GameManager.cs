1022df874b4f9df9505b1092759b962795578e2d
syaiful <syaiful919@gmail.com>
2022-03-22 14:29:14 +0700

    quit app when back button pressed

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -9,6 +9,13 @@ public class GameManager : MonoBehaviour
 
     private int countNumber = 0;
 
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+    
     public void Count()
     {
         countNumber++;
